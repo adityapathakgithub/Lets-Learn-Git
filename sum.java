dc43e9f78c684d604758d6c55337b394b121d7d4
Aditya Pathak <adityapathakgithub>
2023-05-04 00:22:02 +0530

    added Multiply.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class sum {
     public static void main(String args[]) {
         int a = 10;
         int b = 10;
-        System.out.print(a + b);
+        System.out.print("The sum is :" + a + b);
     }
 }
